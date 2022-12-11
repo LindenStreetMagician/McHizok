4b6c0007a7cd36bfae74b48c5a0ea760021e6418
Kristóf Seregley <kristof_seregley@epam.com>
2022-12-11 16:27:05 +0100

    Add migrations

---
+++ McHizok.Web/Migrations/20221210235657_AddRoles.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace McHizok.Web.Migrations
+{
+    public partial class AddRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "045dcd8f-6d98-4813-a106-0071f59ae1a9", "e8504a17-4db7-42a4-8e40-f6e963ead6a0", "Admin", "ADMIN" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "045dcd8f-6d98-4813-a106-0071f59ae1a9");
+        }
+    }
+}
