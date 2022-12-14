0dd4733afbea679c3e32e08801fd357c9b801686
Kristóf Seregley <kristof_seregley@epam.com>
2022-12-14 18:53:20 +0100

    Add UserForRegistrationDto

---
+++ McHizok.Entities/DataTransferObjects/UserForRegistrationDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace McHizok.Entities.DataTransferObjects;
+
+public record UserForRegistrationDto
+{
+    [Required(ErrorMessage = "A felhasználó név megadása kötelező")]
+    public string? UserName { get; init; }
+
+    [Required(ErrorMessage = "A jelszó megadása kötelező")]
+    public string? Password { get; init; }
+
+    public ICollection<string>? Roles { get; init; }
+}
