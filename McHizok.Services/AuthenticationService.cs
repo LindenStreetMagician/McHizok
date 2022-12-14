f496b955766462190fd06c1cc3644c48eb58bb43
Kristóf Seregley <kristof_seregley@epam.com>
2022-12-14 19:07:08 +0100

    Add AuthenticationService

---
+++ McHizok.Services/AuthenticationService.cs
@@ -0,0 +1,31 @@
+﻿using McHizok.Entities.DataTransferObjects;
+using McHizok.Entities.Models;
+using McHizok.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace McHizok.Services;
+
+public class AuthenticationService : IAuthenticationService
+{
+    private readonly UserManager<User> _userManager;
+
+    public AuthenticationService(UserManager<User> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistration)
+    {
+        var user = new User
+        {
+            UserName = userForRegistration.UserName
+        };
+
+        var result = await _userManager.CreateAsync(user, userForRegistration.Password);
+
+        if (result.Succeeded)
+            await _userManager.AddToRolesAsync(user, userForRegistration.Roles);
+
+        return result;
+    }
+}
---
+++ McHizok.Services/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,9 @@
+﻿using McHizok.Entities.DataTransferObjects;
+using Microsoft.AspNetCore.Identity;
+
+namespace McHizok.Services.Interfaces;
+
+public interface IAuthenticationService
+{
+    Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistration);
+}
