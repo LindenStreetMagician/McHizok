25edabee1f9c2e67adadec2a98f32788e9effbdd
Kristóf Seregley <kristof_seregley@epam.com>
2022-12-14 19:27:35 +0100

    Add RegisterUser action to AuthenticationController

--- McHizok.Web/Controllers/AuthenticationController.cs
+++ McHizok.Web/Controllers/AuthenticationController.cs
@@ -1,6 +1,39 @@
-﻿namespace McHizok.Web.Controllers
+﻿using McHizok.Entities.DataTransferObjects;
+using McHizok.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace McHizok.Web.Controllers;
+
+[Route("api/authentication")]
+[ApiController]
+public class AuthenticationController : ControllerBase
 {
-    public class AuthenticationController
+    private readonly IAuthenticationService _authenticationService;
+    public AuthenticationController(IAuthenticationService authenticationService)
     {
+        _authenticationService = authenticationService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> RegisterUser([FromBody] UserForRegistrationDto userForRegistration)
+    {
+        if (userForRegistration is null)
+            return BadRequest("userForRegistrationDto cannot be null.");
+
+        if (!ModelState.IsValid)
+            return UnprocessableEntity(ModelState);
+
+        var result = await _authenticationService.RegisterUser(userForRegistration);
+
+        if (!result.Succeeded)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.TryAddModelError(error.Code, error.Description);
+            }
+            return BadRequest(ModelState);
+        }
+
+        return StatusCode(201);
     }
 }
