950c2e23eca3dabf269e5c04de63ba32dfbd2003
Kristóf Seregley <kristof_seregley@epam.com>
2023-01-04 18:06:25 +0100

    Add Routing

--- McHizok.Web/ClientApp/src/app/app-routing.module.ts
+++ McHizok.Web/ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ApplePieComponent } from './apple-pie/apple-pie.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'pies', component: ApplePieComponent },
+  { path: '**', component: LoginComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
