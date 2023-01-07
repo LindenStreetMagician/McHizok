1b7f9b652e26e3ca780378ca6c3e5f8d9326f65d
Kristóf Seregley <kristof_seregley@epam.com>
2023-01-07 22:19:15 +0100

    Add AccountsController stub

---
+++ McHizok.Web/Controllers/AccountsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace McHizok.Web.Controllers;
+
+[Route("/api/accounts")]
+[ApiController]
+[Authorize(Roles = "Admin")]
+public class AccountsController : ControllerBase
+{
+
+    public AccountsController()
+    {
+    }
+
+    [HttpGet("generate")]
+    public async Task<IActionResult> CreateRegistrationLink()
+    {
+        return Ok("CreateRegLink");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetUsers()
+    {
+        return Ok("GetUsers");
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> DeleteUser(Guid id)
+    {
+        return NoContent();
+    }
+
+
+}
