2c00e1737adba0c8723b699bf29803218f43b535
Kristóf Seregley <kristof_seregley@epam.com>
2023-01-08 21:44:18 +0100

    Add 'api/users/generate' call to user.service

---
+++ McHizok.Web/ClientApp/src/app/services/user.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private _baseUrl: string = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  generateRegistrationLink(to: string): Observable<string> {
+    return this.http.get(this._baseUrl + 'api/users/generate',
+      {
+        responseType: "text",
+        params: new HttpParams().set('to', to)
+      }
+    );
+  }
+}
