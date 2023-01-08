f77a421daec1d1083e40b4efd2962dd065dfa194
Kristóf Seregley <kristof_seregley@epam.com>
2023-01-08 21:59:51 +0100

    Extend generate-register-link.component functions

--- McHizok.Web/ClientApp/src/app/components/generate-register-link/generate-register-link.component.ts
+++ McHizok.Web/ClientApp/src/app/components/generate-register-link/generate-register-link.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { UserService } from 'src/app/services/user.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-generate-register-link',
@@ -6,5 +9,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./generate-register-link.component.css']
 })
 export class GenerateRegisterLinkComponent {
+  to: string = '';
+  registrationToken: string = '';
+  registrationUrl: string = '';
+  constructor(private toastr: ToastrService, private userService: UserService, private clipboard: Clipboard) { }
 
+  onClickGenerate() {
+    if (this.to == "") {
+      this.toastr.error('Missing for who!');
+      return
+    }
+
+    this.userService.generateRegistrationLink(this.to).subscribe({
+      next: (regToken) => {
+        this.registrationToken = regToken;
+        this.registrationUrl = `${location.origin}/register/${this.registrationToken}`;
+
+        this.copyUrlToClipboard(this.registrationUrl);
+      }
+    });
+  }
+
+  reCopyUrl(tokenText: any) {
+    if (tokenText.value == "") {
+      return;
+    }
+
+    this.copyUrlToClipboard(this.registrationUrl);
+  }
+
+  private copyUrlToClipboard(url: string) {
+    let copyResult = this.clipboard.copy(this.registrationUrl);
+
+    if (copyResult) {
+      this.toastr.success('Link copied to clipboard', `${this.to} reg link`);
+    } else {
+      this.toastr.error('Copy failed.');
+    }
+  }
 }
