4b248d6c1bf330d15451a94bcc5f1a3d42e2191e
Kristóf Seregley <kristof_seregley@epam.com>
2023-01-10 00:09:01 +0100

    Add NotFoundException and UserNotFoundException

---
+++ McHizok.Entities/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace McHizok.Entities.Exceptions;
+
+public class UserNotFoundException : NotFoundException
+{
+    public UserNotFoundException(string userId) 
+        : base($"The user with id: {userId} doesn't exist in the database")
+    {
+    }
+}
