5ffa6d56490f821b9784a56e5960ba38125da015
Kristóf Seregley <kristof_seregley@epam.com>
2023-01-11 09:09:24 +0100

    Add GetUsers service call

--- McHizok.Web/ClientApp/src/app/components/user-management/user-management.component.ts
+++ McHizok.Web/ClientApp/src/app/components/user-management/user-management.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-management',
   templateUrl: './user-management.component.html',
   styleUrls: ['./user-management.component.css']
 })
-export class UserManagementComponent {
+export class UserManagementComponent implements OnInit {
+  Users: User[] = [];
 
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe({
+      next: (users) => {
+        this.Users = users;
+      }
+    });
+  }
+
+  onClickDelete(userId: string) {
+    console.log(userId);
+  }
 }
--- McHizok.Web/ClientApp/src/app/services/user.service.ts
+++ McHizok.Web/ClientApp/src/app/services/user.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { RegisterRequest } from '../models/register-request.model';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -30,5 +31,9 @@ export class UserService {
 
   registerUser(registerRequest: RegisterRequest) {
     return this.http.post(this._baseUrl + 'api/users/register', registerRequest);
+  }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this._baseUrl + "api/users");
   }
 }
