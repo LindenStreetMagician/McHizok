1411d3d111e779cf9806a1510fa4615834925693
Kristóf Seregley <kristof_seregley@epam.com>
2023-01-17 20:09:02 +0100

    Add CouponNotFoundException

---
+++ McHizok.Entities/Exceptions/CouponNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace McHizok.Entities.Exceptions;
+
+public class CouponNotFoundException : NotFoundException
+{
+    public CouponNotFoundException(Guid couponId) 
+        : base($"The coupon with the Id: {couponId} doesn't exist")
+    {
+    }
+}
