da9fd839bae846c3a60a437f17a03b3af91c2e27
Kristóf Seregley <kristof_seregley@epam.com>
2023-01-17 20:59:48 +0100

    Add FileName to CouponInventory

--- McHizok.Web/Data/CouponInventory.cs
+++ McHizok.Web/Data/CouponInventory.cs
@@ -6,6 +6,7 @@ namespace McHizok.Web.Data;
 public class CouponInventory
 {
     public Guid Id { get; set; }
+    public string FileName { get; set; }
     public string CouponBase64 { get; set; }
     public string CouponCode { get; set; }
     public DateTime ExpiresAt { get; set; } = DateTime.Now.AddDays(7);
