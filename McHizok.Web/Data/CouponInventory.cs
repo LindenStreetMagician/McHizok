e2e5d03722335aa6cda213b7e3ee48190d24577f
Kristóf Seregley <kristof_seregley@epam.com>
2023-01-17 17:47:24 +0100

    Add CouponInventory.cs

---
+++ McHizok.Web/Data/CouponInventory.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace McHizok.Web.Data;
+
+[Table("CouponInventory")]
+public class CouponInventory
+{
+    public Guid Id { get; set; }
+    public string CouponBase64 { get; set; }
+    public string CouponCode { get; set; }
+    public DateTime ExpiresAt { get; set; } = DateTime.Now.AddDays(7);
+
+}
