30d5e23f43528312f30edb522b76cc5061500caa
Kristóf Seregley <kristof_seregley@epam.com>
2023-01-20 13:21:16 +0100

    Add coupon-inventory.component

---
+++ McHizok.Web/ClientApp/src/app/components/coupon-inventory/coupon-inventory.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coupon-inventory',
+  templateUrl: './coupon-inventory.component.html',
+  styleUrls: ['./coupon-inventory.component.css']
+})
+export class CouponInventoryComponent {
+
+}
