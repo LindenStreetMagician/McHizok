cb80532f57e1ad28d1c0ee3de586cced2c668478
Kristóf Seregley <kristof_seregley@epam.com>
2023-01-22 21:06:58 +0100

    Disable Application Insights dependency tracker

--- McHizok.Web/Program.cs
+++ McHizok.Web/Program.cs
@@ -8,7 +8,7 @@ builder.Services.Configure<ApiBehaviorOptions>(options =>
     options.SuppressModelStateInvalidFilter = true;
 });
 
-builder.Services.AddApplicationInsightsTelemetry();
+builder.Services.AddApplicationInsightsTelemetry(options => options.EnableDependencyTrackingTelemetryModule = false );
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddCors();
