dd6db781ec5088bc3bb912bf08d2302ea30ae654
Kristóf Seregley <kristof_seregley@epam.com>
2023-01-23 23:57:10 +0100

    Remove excess usings

--- McHizok.Entities/Exceptions/RegistrationTokenNotProvidedBadRequestException.cs
+++ McHizok.Entities/Exceptions/RegistrationTokenNotProvidedBadRequestException.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace McHizok.Entities.Exceptions;
+﻿namespace McHizok.Entities.Exceptions;
 
 public class RegistrationTokenNotProvidedBadRequestException : BadRequestException
 {
